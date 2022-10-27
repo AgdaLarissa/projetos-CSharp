a81f07c7e36e67eca1ff782cf7ee6559ad8e8c59
Agda <agdaafram@gmail.com>
2022-10-27 20:46:48 -0300

    Alterações no método DrawScreen()

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -9,7 +9,7 @@ namespace EditorHTML
             public static void Show(){
                 Console.Clear();
 
-                Console.BackgroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Blue;
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 DrawScreen();
@@ -22,32 +22,40 @@ namespace EditorHTML
 
         public static void DrawScreen()
             {
-                Console.Write("#0");
-                for(int i=0; i<=30; i++)
-                    Console.Write("-");
-                
-                Console.Write("+");
-                Console.Write("\n");
 
-                for (int lines=0; lines<=10; lines++)
+                LinhaSuperiorInferiorHorizontal();
+
+                LinhaDireitaEsquerdaBlocoVertical();
+
+                LinhaSuperiorInferiorHorizontal();
+
+                void LinhaSuperiorInferiorHorizontal()
                 {
-                    Console.Write("|");
+                    Console.Write("+");
                     for(int i=0; i<=30; i++)
-                        Console.Write(" ");
-
-                    Console.Write("|");
+                        Console.Write("-");
+                
+                    Console.Write("+");
                     Console.Write("\n");
                 }
 
-                Console.Write("+");
-                for(int i=0; i<=30; i++)
-                    Console.Write("-");
-                
-                Console.Write("+");
-                Console.Write("\n");
+
+                void LinhaDireitaEsquerdaBlocoVertical()
+                {
+                    for (int lines=0; lines<=10; lines++)
+                    {
+                        Console.Write("|");
+                        for(int i=0; i<=30; i++)
+                            Console.Write(" ");
+
+                        Console.Write("|");
+                        Console.Write("\n");
+                }
+
+                }
             }
 
-            public static void WriteOptions()
+        public static void WriteOptions()
             {
                 Console.SetCursorPosition(3,2);
                 Console.WriteLine("Editor HTML");
